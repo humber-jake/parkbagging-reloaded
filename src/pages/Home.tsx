81930452fb6c2086a3a703c96624e7def1258648
humber-jake <humber.jake@gmail.com>
2026-07-15 22:57:53 -0700

    style changes and teaser page, favicon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,8 @@ interface HomeProps {
   ride: Ride | null;
 }
 
+const teaser = true;
+
 export default function Home({ ride }: HomeProps) {
   let distance = 0;
 
@@ -21,38 +23,45 @@ export default function Home({ ride }: HomeProps) {
   }
 
   return (
-    <div className="home">
-      <h1 className="logo">PARKBAGGING</h1>
-      <h1 className="logo">RELOADED</h1>
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className="ride-card">
-        <h2 className="date">
-          {" "}
-          {ride ? formatDate(ride.date) : "No date selected"}
-        </h2>
+    <div className={teaser ? "home-teaser" : "home"}>
+      <img className="logo-image" src="/logo.png" alt="Parkbagging-reloaded" />
 
-        <div className="parks">
-          {ride ? (
-            <>
-              <h2>{ride.park1.name}</h2>
-              <h2>{ride.park2.name}</h2>
-            </>
-          ) : (
-            <h2>No parks selected yet</h2>
-          )}
+      {teaser && (
+        <>
+          <div className="teaser-overlay">
+            <div className="continue">
+              to be CONTINUED.... 🚲 🍻 ?<span className="cursor">_</span>
+            </div>
+          </div>
+        </>
+      )}
+      {!teaser && (
+        <div className="ride-card">
+          <h2 className="date">
+            {" "}
+            {ride ? formatDate(ride.date) : "No date selected"}
+          </h2>
+
+          <div className="parks">
+            {ride ? (
+              <>
+                <h2>{ride.park1.name}</h2>
+                <h2>{ride.park2.name}</h2>
+              </>
+            ) : (
+              <h2>No parks selected yet</h2>
+            )}
+          </div>
+
+          {ride && <h3 className="length">Ride Length - {distance}km</h3>}
+
+          <p className="time">
+            Meet @ {ride ? formatTime(ride.time) : "--:--"}(ish)
+          </p>
+
+          <div className="emojis">🚲🍻</div>
         </div>
-
-        {ride && <h3 className="length">Ride Length - {distance}km</h3>}
-
-        <p className="time">
-          Meet @ {ride ? formatTime(ride.time) : "--:--"}(ish)
-        </p>
-
-        <div className="emojis">🚲🍻</div>
-      </div>
+      )}
     </div>
   );
 }
